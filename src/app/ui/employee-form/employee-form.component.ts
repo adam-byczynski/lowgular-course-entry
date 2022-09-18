46e74505e1953efd9d6e400a20f98bc1ccd00cea
adam.byczynski <adambyczynski@protonmail.ch>
2022-09-18 10:05:15 +0200

    feat: part2 ex.3: popup window appears to be working?

--- src/app/ui/employee-form/employee-form.component.ts
+++ src/app/ui/employee-form/employee-form.component.ts
@@ -1,5 +1,7 @@
 import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
-import {FormControl, FormGroup, Validators} from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { EmployeeService } from '../../services/employee.service';
+import { CreateEmployeeModel } from '../../model/create-employee.model';
 
 @Component({
   selector: 'app-employee-form',
@@ -14,7 +16,19 @@ export class EmployeeFormComponent {
     salary: new FormControl(null, [Validators.required, Validators.min(0)])
   });
 
-  onButtonClicked(form: { name: string, age: number, salary: number}) {
+  constructor(private _employeeService: EmployeeService) {
+  }
+
+  onFormSubmitted(form: CreateEmployeeModel) {
+    this._employeeService.create(form).subscribe(_ => this.onButtonClicked(form));
+  /*
+  appears to be working intendedly when checked for the api https://dummy.restapiexample.com/api/v1/create,
+  which doesn't raise errors, when checked with the https://dummy.restapiexample.com/create used in the exercise
+  description doesn't show popup window because of some errors: "CORS missing allow origin" and "NS_ERROR_DOM_BAD_URL"
+  */
+  }
+
+  onButtonClicked(form: CreateEmployeeModel) {
     alert(`User was successfully added to the database. Name: ${form.name}, Age: ${form.age}, Salary: ${form.salary}`);
   }
 }
