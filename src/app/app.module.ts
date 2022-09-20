e43f7d9b423c955704159362cae331d1b836419f
adam.byczynski <adambyczynski@protonmail.ch>
2022-09-20 06:27:27 +0200

    feat: part2 exam: previous files deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { EmployeeListComponentModule } from "./ui/employee-list/employee-list.component-module";
-import {
-  EmployeeImagesLowBarComponentModule
-} from "./ui/employee-images-low-bar/employee-images-low-bar.component-module";
-import {EmployeeServiceModule} from "./services/employee.service-module";
-import {ProjectsServiceModule} from "./services/projects.service-module";
-import {ProjectsListComponentModule} from "./ui/projects-list/projects-list.component-module";
-import {EmployeeFormComponentModule} from "./ui/employee-form/employee-form.component-module";
+
 
 @NgModule({
   declarations: [AppComponent],
@@ -18,12 +11,6 @@ import {EmployeeFormComponentModule} from "./ui/employee-form/employee-form.comp
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    EmployeeListComponentModule,
-    EmployeeImagesLowBarComponentModule,
-    EmployeeServiceModule,
-    ProjectsListComponentModule,
-    ProjectsServiceModule,
-    EmployeeFormComponentModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
